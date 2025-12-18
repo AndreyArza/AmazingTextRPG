2c54b3b0ee96141c30b6cf92f2eb3f10569a2ad7
Andrey Arzamatov <a.arzamatov@iva.ru>
2025-12-18 11:57:31 +0300

    Fix design

--- app/src/main/java/ru/andrey/demotextrpg/ui/screen/Splash.kt
+++ app/src/main/java/ru/andrey/demotextrpg/ui/screen/Splash.kt
@@ -2,7 +2,10 @@ package ru.andrey.demotextrpg.ui.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -16,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -32,15 +36,15 @@ fun Splash(
 ) {
     val vm: SplashViewModel = viewModel(factory = LocalViewModelFactory.current)
     val state by vm.state.collectAsStateWithLifecycle()
-    Column(modifier = Modifier.background(White)) {
+    Column(modifier = Modifier.background(White).fillMaxHeight(), verticalArrangement = Arrangement.Center) {
         Image(
-            painterResource(R.mipmap.ic_launcher_foreground),
+            painterResource(R.drawable.ic_launcher),
             contentDescription = "",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.weight(1f)
+            contentScale = ContentScale.Inside,
+            modifier = Modifier.padding(24.dp)
         )
         Text(
-            text = state.toString(),
+            text = stringResource(R.string.app_name),
             modifier = Modifier
                 .padding(
                     bottom = 24.dp
